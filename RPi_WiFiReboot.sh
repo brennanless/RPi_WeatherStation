875f062cbb7ef25749f0c0e8e10d6483399da9ae
brennanless <brennan.less@gmail.com>
2016-12-13 10:58:28 -0800

    Changed reboot script to reboot whole RPi, not just wifi.

--- RPi_WiFiReboot.sh
+++ RPi_WiFiReboot.sh
@@ -1,14 +1,15 @@
 
 #This script is borrowed from http://weworkweplay.com/play/rebooting-the-raspberry-pi-when-it-loses-wireless-connection-wifi/
 #It pings the remote cradle point router (in the Fresno attic), and if the connection is down, the RPi wifi is rebooted, which
-#hopefully leads RPi to reconnect to 'live' network.
+#hopefully leads RPi to reconnect to live network.
 
 ping -c4 192.168.21.1 > /dev/null
- 
-if [ $? != 0 ] 
+
+if [ $? != 0 ]
 then
-  echo "No network connection, restarting wlan0"
-  /sbin/ifdown 'wlan0'
-  sleep 5
-  /sbin/ifup --force 'wlan0'
+  sudo /sbin/shutdown -r now
+  #echo "No network connection, restarting wlan0"
+  #/sbin/ifdown 'wlan0'
+  #sleep 5
+  #/sbin/ifup --force 'wlan0'
 fi
