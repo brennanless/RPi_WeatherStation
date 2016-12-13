cd333c68e7c0ad7e5fecf8b666573f319c05794e
BrennanLess <brennan.less@gmail.com>
2016-12-13 15:29:49 -0800

    Note about the command to enable ping on Raspbian.

--- RPi_WiFiReboot.sh
+++ RPi_WiFiReboot.sh
@@ -2,6 +2,7 @@
 #This script is borrowed from http://weworkweplay.com/play/rebooting-the-raspberry-pi-when-it-loses-wireless-connection-wifi/
 #It pings the remote cradle point router (in the Fresno attic), and if the connection is down, the RPi wifi is rebooted, which
 #hopefully leads RPi to reconnect to live network.
+#Ping may not work on some versions of Raspbian, issue "sudo chmod u+s /bin/ping" to enable ping.
 
 ping -c4 192.168.21.1 > /dev/null
 
